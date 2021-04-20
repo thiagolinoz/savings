6369a51f4def7d92371da4dde997ba34cb7660c7
Thiago Lino <thiagolinoz@gmail.com>
2021-04-20 13:16:55 -0300

    add anotacao Transactional ao salvar

--- src/main/java/com/linoz/savings/service/UserService.java
+++ src/main/java/com/linoz/savings/service/UserService.java
@@ -9,6 +9,7 @@ import com.linoz.savings.repository.UserRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -22,6 +23,7 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
+    @Transactional
     public User addUser(UserPostDTO userPostDTO) {
         if (findUserByEmail(userPostDTO.getEmail()) != null) {
             throw new UserException("e-mail existente na base");
