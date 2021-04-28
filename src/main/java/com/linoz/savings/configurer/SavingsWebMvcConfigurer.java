babcc869f719b38555a4ff9b492a14190fc718d7
Thiago Lino <thiagolinoz@gmail.com>
2021-04-28 08:02:56 -0300

    add valor default para paginacao com WebMvcConfigurer

---
+++ src/main/java/com/linoz/savings/configurer/SavingsWebMvcConfigurer.java
@@ -0,0 +1,23 @@
+package com.linoz.savings.configurer;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
+ * Created by linoz on 4/28/21
+ */
+@Configuration
+public class SavingsWebMvcConfigurer implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        PageableHandlerMethodArgumentResolver pageHandler = new PageableHandlerMethodArgumentResolver();
+        pageHandler.setFallbackPageable(PageRequest.of(0, 5));
+        resolvers.add(pageHandler);
+    }
+}
